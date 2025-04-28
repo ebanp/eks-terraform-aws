b0bbb83d4fb285b5517f30bfdf58e22fcfda6901
ebanp <88750256+ebanp@users.noreply.github.com>
2025-04-28 17:04:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = var.region
   profile    = "default"
-  access_key = ""
-  secret_key = ""
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 terraform {
