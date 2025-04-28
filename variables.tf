e334e40804322df21f8b2d793db696186ea5b7e9
ebanp <88750256+ebanp@users.noreply.github.com>
2025-04-28 18:34:04 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,3 +18,43 @@ variable "AWS_SECRET_ACCESS_KEY" {
   description = var.AWS_SECRET_ACCESS_KEY
   type        = string
 }
+
+variable "namespace" {
+  type    = string
+  default = "hola-mundo"
+}
+
+variable "app_name" {
+  type = string
+  default = "hola-mundo-app" 
+}
+
+variable "replicas" {
+  type    = number
+  default = 1
+}
+
+variable "container_port" {
+  type    = number
+  default = 8080
+}
+
+variable "service_port" {
+  type    = number
+  default = 80
+}
+
+variable "service_type" {
+  type    = string
+  default = "LoadBalancer"
+}
+
+variable "app_image" {
+  type    = string
+  default = "hashicorp/http-echo:0.2.3"
+}
+
+variable "app_args" {
+  type    = list(string)
+  default = ["-text=Hola Mundo"]
+}
