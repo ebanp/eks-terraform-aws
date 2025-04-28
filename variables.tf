b664a5d5bf551fe973cfe705c7520c5ea08353f8
ebanp <88750256+ebanp@users.noreply.github.com>
2025-04-28 17:04:23 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,20 @@
 variable "region" {
-  description = "AWS region"
+  description = var.AWS_REGION
   type        = string
-  default     = "us-east-2"
 }
 
 variable "vpc_name" {
   description = "AWS vpc"
   type        = string
   default     = "eks-terraform-vpc"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = var.AWS_ACCESS_KEY_ID
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = var.AWS_SECRET_ACCESS_KEY
+  type        = string
 }
