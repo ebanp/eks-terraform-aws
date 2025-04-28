ba2fdfd52a9f682beda94065cc436d11133fdba1
ebanp <88750256+ebanp@users.noreply.github.com>
2025-04-28 18:38:58 -0500

    Delete HelloWorld directory

--- HelloWorld/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "service_name" {
-  value = kubernetes_service.this.metadata[0].name
-}
-
-output "load_balancer_hostname" {
-  value = kubernetes_service.this.status[0].load_balancer[0].ingress[0].hostname
-  description = "The hostname of the LoadBalancer."
-}
